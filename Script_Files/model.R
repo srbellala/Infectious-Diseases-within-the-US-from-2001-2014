bbbddbd7e3a218d239a4e4539a3867f6f517980d
srbellala <srbellala@gmail.com>
2023-07-09 22:16:30 -0500

    rough draft

--- Script_Files/model.R
+++ Script_Files/model.R
@@ -2,6 +2,10 @@ library(tidyverse)
 library(ggthemes)
 library(plotly)
 library(rstanarm)
+library(gtsummary)
+library(ggthemr)
+ggthemr("fresh")
+lighten_swatch(amount = .1)
 
 x <- read_csv("Infectious Disease 2001-2014.csv", show_col_types = FALSE)
 
@@ -16,6 +20,8 @@ fit_obj <- stan_glm(data = my_data,
                     refresh = 0,
                     seed = 9)
 
+t1 <- tbl_regression(fit_obj, exponentiate = TRUE)
+
 new_obs <- tibble(Sex = "Male")
 
 pe <- posterior_predict(fit_obj, new_data = new_obs) |>
@@ -25,12 +31,12 @@ model_plot <- pe |>
   ggplot(aes(x = abs(`1`))) +
   geom_histogram(aes(y = after_stat(count/sum(count))), bins = 100) +
   scale_y_continuous(labels = scales::percent_format()) +
-  theme_classic() +
   labs(
     title = "Posterior for E. Coli 0185 infected Males ",
-    subtitle = "21% probability that 5 males are infected with E. Coli 0185",
+    subtitle = "21% probability that more than 5 males are infected with E. Coli 0185",
     x = "Male Count",
     y = "Probability"
   )
 
 write_rds(model_plot, "Model.rds")
+write_rds(t1, "Table.rds")
